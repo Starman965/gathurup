d62e4591d012dd03a747f6dee4756ed9481758b9
David Lewis <107167236+Starman965@users.noreply.github.com>
2024-11-19 12:58:37 -1000

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts('https://www.gstatic.com/firebasejs/10.4.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.4.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    // Copy your firebase config here
+    ...firebaseConfig
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('Received background message:', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/gathurUP.png'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
