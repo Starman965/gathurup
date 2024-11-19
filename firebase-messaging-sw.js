2591f28d981eae6579a6efc4137ed0a44f093d12
David Lewis <107167236+Starman965@users.noreply.github.com>
2024-11-19 13:01:44 -1000

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,8 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/10.4.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.4.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    // Copy your firebase config here
-    ...firebaseConfig
+const firebaseConfig = {
+    apiKey: "AIzaSyDmuTmID03W_985LWLRkHZZVVsKEET_s48",
+    authDomain: "eventbrighter-22f02.firebaseapp.com",
+    projectId: "eventbrighter-22f02",
+    storageBucket: "eventbrighter-22f02.appspot.com",
+    messagingSenderId: "852808214445",
+    appId: "1:852808214445:web:1f4c59a5ba9a56891ee324"
+};
 });
 
 const messaging = firebase.messaging();
